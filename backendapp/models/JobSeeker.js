d0b79b3482c1ee87f59a923894eb753ead2532f7
Your Name <you@example.com>
2024-04-27 08:18:36 +0530

    code changes

---
+++ backendapp/models/JobSeeker.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose")
+
+const jobseekerschema = new mongoose.Schema({
+    fullname: {
+      type: String,
+      required: true,
+    },
+    gender: {
+      type: String,
+      required:true,
+      enum: ['male', 'female', 'others']
+    },
+    dateofbirth: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    location: {
+      type: String,
+      required: true
+    },
+    contact: {
+        type: String,
+        required: true,
+        unique:true
+      },
+
+      university: {
+        type: String,
+        required: true,
+        
+      },
+      universityID: {
+        type: String,
+        required: true,
+        
+      },
+  });
+
+const jobseeker = mongoose.model('jobseeker', jobseekerschema);
+
+module.exports = jobseeker;
